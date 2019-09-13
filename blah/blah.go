0f15d0f8770f74d7c24407d68b7c8877fd361f13
Adam Wolfe Gordon <awg@xvx.ca>
2019-09-13 10:33:18 -0600

    Fix lint errors

--- blah/blah.go
+++ blah/blah.go
@@ -2,14 +2,17 @@ package blah
 
 import "fmt"
 
+// Foo is a struct.
 type Foo struct {
 	name string
 }
 
+// NewFoo returns a Foo.
 func NewFoo(name string) *Foo {
 	return &Foo{name}
 }
 
+// Dance monkey dance.
 func (f *Foo) Dance(times int) {
 	for i := 0; i < times; i++ {
 		fmt.Println(f.name)
